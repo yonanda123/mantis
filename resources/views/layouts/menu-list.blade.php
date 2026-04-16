b8177c4675a4642273aca1dfd1d6f516727edf19
Yonanda Haryono <81000399+yonanda123@users.noreply.github.com>
2026-04-16 11:32:01 +0700

    add init menu list

--- resources/views/layouts/menu-list.blade.php
+++ resources/views/layouts/menu-list.blade.php
@@ -6,104 +6,27 @@
 </li>
 
 <li class="pc-item pc-caption">
-    <label>UI Components</label>
-    <i class="ti ti-dashboard"></i>
+    <label>ERP</label>
+    <i class="ti ti-package"></i>
 </li>
 
 <li class="pc-item">
     <a href="#" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-typography"></i></span>
-        <span class="pc-mtext">Typography</span>
+        <span class="pc-micon"><i class="ti ti-box"></i></span>
+        <span class="pc-mtext">Product</span>
     </a>
 </li>
 
 <li class="pc-item">
     <a href="#" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-color-swatch"></i></span>
-        <span class="pc-mtext">Color</span>
+        <span class="pc-micon"><i class="ti ti-category"></i></span>
+        <span class="pc-mtext">Category</span>
     </a>
 </li>
 
 <li class="pc-item">
     <a href="#" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-plant-2"></i></span>
-        <span class="pc-mtext">Icons</span>
-    </a>
-</li>
-
-<li class="pc-item pc-caption">
-    <label>Pages</label>
-    <i class="ti ti-news"></i>
-</li>
-
-<li class="pc-item">
-    <a href="#" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-lock"></i></span>
-        <span class="pc-mtext">Login</span>
-    </a>
-</li>
-
-<li class="pc-item">
-    <a href="#" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-user-plus"></i></span>
-        <span class="pc-mtext">Register</span>
-    </a>
-</li>
-
-<li class="pc-item pc-caption">
-    <label>Other</label>
-    <i class="ti ti-brand-chrome"></i>
-</li>
-
-<li class="pc-item pc-hasmenu">
-    <a href="#!" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-menu"></i></span>
-        <span class="pc-mtext">Menu levels</span>
-        <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
-    </a>
-
-    <ul class="pc-submenu">
-        <li class="pc-item">
-            <a class="pc-link" href="#!">Level 2.1</a>
-        </li>
-
-        <li class="pc-item pc-hasmenu">
-            <a href="#!" class="pc-link">
-                Level 2.2
-                <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
-            </a>
-            <ul class="pc-submenu">
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                <li class="pc-item pc-hasmenu">
-                    <a href="#!" class="pc-link">
-                        Level 3.3
-                        <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
-                    </a>
-                    <ul class="pc-submenu">
-                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-
-        <li class="pc-item pc-hasmenu">
-            <a href="#!" class="pc-link">
-                Level 2.3
-                <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
-            </a>
-            <ul class="pc-submenu">
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-            </ul>
-        </li>
-    </ul>
-</li>
-
-<li class="pc-item">
-    <a href="#" class="pc-link">
-        <span class="pc-micon"><i class="ti ti-brand-chrome"></i></span>
-        <span class="pc-mtext">Sample page</span>
+        <span class="pc-micon"><i class="ti ti-shopping-cart"></i></span>
+        <span class="pc-mtext">Order</span>
     </a>
 </li>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,18 +12,6 @@
             <ul class="pc-navbar">
                 @include('layouts.menu-list')
             </ul>
-
-            <div class="card text-center">
-                <div class="card-body">
-                    <img src="{{ asset('assets/images/img-navbar-card.png') }}" alt="images" class="img-fluid mb-2">
-                    <h5>Upgrade To Pro</h5>
-                    <p>To get more features and components</p>
-                    <a href="https://codedthemes.com/item/berry-bootstrap-5-admin-template/" target="_blank"
-                        class="btn btn-success">
-                        Buy Now
-                    </a>
-                </div>
-            </div>
         </div>
     </div>
 </nav>
