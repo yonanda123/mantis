5da97ac55c3b6c1b7da0089dd4c481a57e59808b
Yonanda Haryono <81000399+yonanda123@users.noreply.github.com>
2026-04-17 07:32:55 +0700

    add login & register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::share([
+            'pc_dark_layout' => 'default',
+            'pc_box_container' => 'false',
+            'pc_rtl_layout' => 'false',
+            'pc_preset_theme' => '',
+            'font_name' => '',
+            'breadcrumb_item' => '',
+            'breadcrumb_item_active' => 'Dashboard'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard', [
-        'pc_dark_layout' => 'default',
-        'pc_box_container' => 'false',
-        'pc_rtl_layout' => 'false',
-        'pc_preset_theme' => '',
-        'font_name' => '',
-        'breadcrumb_item' => '',
-        'breadcrumb_item_active' => 'Dashboard'
-    ]);
-});
+Route::get('/', fn() => view('login'));
+Route::get('/register', fn() => view('register'));
+Route::get('/dashboard', fn() => view('dashboard'));
